998c686981762a698f8d41b049bc24340266207d
Ifeanyi Ani <ifeanyiani155@gmail.com>
2025-02-02 12:03:37 +0100

    add more security to the express app

--- server/app.js
+++ server/app.js
@@ -1,12 +1,30 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const xssClean = require("xss-clean");
+const mongoSanitizer = require("express-mongo-sanitize");
+const rateLimit = require("express-rate-limit");
+
 const globalError = require("./controllers/globalErrorController");
 const referralRoutes = require("./routes/referralRoutes");
 const AppError = require("./utils/AppError");
 
+const limiter = rateLimit({
+  max: 200,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests for this IP, please try again in an hour",
+});
+
 const app = express();
 
-app.listen(4000, () => {
-  console.log("app is running on port 4000");
+app.use(helmet());
+app.use(limiter);
+app.use(mongoSanitizer());
+app.use(xssClean());
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/codes", referralRoutes);
 app.all("*", (req, _res, next) => {
