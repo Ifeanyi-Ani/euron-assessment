22cb101fa14513670a2d119a40403c3fad79a6a0
Ifeanyi Ani <ifeanyiani155@gmail.com>
2025-02-02 14:19:01 +0100

    server-final

--- server/controllers/referralControllers.js
+++ server/controllers/referralControllers.js
@@ -5,26 +5,18 @@ const catchAsyncError = require("../utils/catchAsyncError");
 exports.createReferral = catchAsyncError(async (req, res, next) => {
   const { address, ref_code, is_manager_code } = req.body;
 
-  if (!address || !ref_code) {
-    return next(new AppError("Missing required fields", 400));
-  }
-
   const count = await Referral.countDocuments({
     address,
   });
 
   if (count >= 10) {
-    return res.status(400).json({ error: "Maximum referral limit reached" });
+    return next(new AppError("Maximum referral limit reached"));
   }
 
-  const existingReferral = await Referral.findOne({ ref_code });
-  if (existingReferral) {
-    return next(new AppError("Referral code already exists", 400));
-  }
+  const existingReferralCode = await Referral.findOne({ ref_code });
 
-  const userExistingReferral = await Referral.findOne({ address, ref_code });
-  if (userExistingReferral) {
-    return next(new AppError("You have already used this referral code", 400));
+  if (existingReferralCode) {
+    return next(new AppError("Refarral code already exist"));
   }
 
   const referral = await Referral.create({
